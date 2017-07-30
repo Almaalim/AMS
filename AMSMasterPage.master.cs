2f54c1bd5197cf568a0004f05db7bfe25b2a4b23
ALMAALIM\m.dassan <m.dassan@almaalim.com>
2017-07-31 00:42:24 +0300

    muath:fix arabic menu

--- AMSMasterPage.master.cs
+++ AMSMasterPage.master.cs
@@ -243,7 +243,7 @@ public partial class AMSMasterPage : System.Web.UI.MasterPage
     {
 
         string lang = (pgCs.Lang == "AR") ? "Ar" : "En";
-        string q = " SELECT MnuID AS ID FROM Menu WHERE MnuText" + lang + " = '" + e.Item.Text + "'";
+        string q = " SELECT MnuTextEn AS ID FROM Menu WHERE MnuText" + lang + " = '" + e.Item.Text + "'";
         DataTable DT1 = DBCs.FetchData(new SqlCommand(q));
         if (!DBCs.IsNullOrEmpty(DT1))
         {
@@ -252,7 +252,9 @@ public partial class AMSMasterPage : System.Web.UI.MasterPage
                 string menuCss = DT1.Rows[0]["ID"].ToString();
                 menuCss = menuCss.Replace(" ", "");
                 menuCss = Regex.Replace(menuCss, @"[^0-9a-zA-Z]+", "");
-                e.Item.ToolTip = "SideMenuItem " + "icon" + menuCss;
+                e.Item.ToolTip = menuCss;
+              //  e.Item.ToolTip = "SideMenuItem " + "icon" + menuCss;
+               // e.Item.ToolTip = "SideMenuItem " + "icon448";
             }
         }
 
